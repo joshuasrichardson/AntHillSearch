579ec3487e0065e68924d5c52579614779ecbbd7
Joshua Richardson <joshuasrichardson1@gmail.com>
2022-04-04 13:10:23 -0600

    Generate river flood zones

---
+++ test/model/FloodZone.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+
+def PolyArea(x, y):
+    return 0.5 * np.abs(np.dot(x, np.roll(y, 1)) - np.dot(y, np.roll(x, 1)))
+
+x = np.arange(0,1,0.001)
+y = np.sqrt(1-x**2)
+print(f"{x}")
+print(PolyArea(x, y))
+
+points = [[0, 0], [-610, 610], [620, 620], [610, 610], [710, 710]]
+x = []
+y = []
+for point in points:
+    x.append(point[0])
+    y.append(point[1])
+    print(f"{x}")
+
+from shapely.geometry import Polygon
+pgon = Polygon(zip(x, y))  # Assuming the OP's x,y coordinates
+
+print(pgon.area)
